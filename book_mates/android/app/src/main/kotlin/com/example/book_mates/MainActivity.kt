cda0fc3cc107f618273620a3d20d94d1f24bb8db
Faris <fariskazi019@gmail.com>
2023-09-20 22:06:50 -0500

    made flutter project

--- book_mates/android/app/src/main/kotlin/com/example/book_mates/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.book_mates
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ bookmates/android/app/src/main/kotlin/com/example/bookmates/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bookmates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
