1ee87d050bdd98109073098df6f75e76fb78ebaf
Bubbq <soccer17nemo@gmail.com>
2023-11-07 22:50:01 -0600

    basically done w screen, need to refactor and remove background from png image

--- lib/PDF Upload/pdf_service.dart
+++ lib/PDF Upload/pdf_service.dart
@@ -1,5 +1,6 @@
 import 'package:bookmates_app/Group%20Operations/group_repo.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
@@ -7,7 +8,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PDFViewer extends StatelessWidget {
   final String pdfUrl;
-
   const PDFViewer(this.pdfUrl, {super.key});
 
   @override
@@ -57,5 +57,34 @@ class RecentlyRead {
   static Future<void> clearHistory() async {
     final email = FirebaseAuth.instance.currentUser?.email;
     subDelete('users/$email/BookPDFs');
+  }
+}
+
+Future<String?> pickPDF() async {
+  //sending user to drive to choose file
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: ['pdf'], // only allowing pdfs for now
+  );
+
+  if (result != null) {
+    return result.files.single.path;
+  } else {
+    return null;
+  }
+}
+
+Future<void> pickAndDisplayPDF() async {
+  // to update the filepath to view in app
+  final email = FirebaseAuth.instance.currentUser?.email;
+  final pdfPath = await pickPDF();
+  final pdfName = pdfPath?.split('/').join();
+  final docID = pdfName?.substring(50, pdfName.length - 4);
+  final pdfRef = FirebaseFirestore.instance.collection('users/$email/BookPDFs');
+  if (pdfPath != null) {
+    await pdfRef.doc(docID).set({
+      'filePath': pdfPath, // actual path in user's device to access
+      'timeStamp': DateTime.now(), // to be able to store history in order
+    });
   }
 }
